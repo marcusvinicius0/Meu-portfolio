30041070858e6e1451dc31749cacc2a4af651704
Marcus Vinícius <begheli2020@gmail.com>
2022-08-02 01:26:09 -0300

    feat: scroll bar animation, image animation, scroll bar costumized

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import ScrollReveal from 'scrollreveal';
 import Header from "./Components/Header";
 import Introduction from "./Components/Introduction";
 import AboutMe from "./Components/AboutMe";
@@ -7,26 +7,45 @@ import MyProjects from "./Components/MyProjects";
 import MyStack from "./Components/MyStack";
 
 import Footer from "./Components/Footer";
+import { useEffect } from "react";
 
 function App() {
+
+  useEffect(() => {
+    ScrollReveal({ reset: true })
+
+    ScrollReveal({
+      distance: '50px',
+      duration: 2000,
+      origin: 'top',
+      reset: false
+    })
+
+
+    ScrollReveal().reveal('.d-200', { delay: 200 })
+    ScrollReveal().reveal('.d-300', { delay: 300 })
+    ScrollReveal().reveal('.d-400', { delay: 400 })
+
+  }, []);
+
   return (
     <>
       <Header />
 
       <main>
-        <article>
+        <article className='d-200'>
           <Introduction />
         </article>
 
-        <article>
+        <article className='d-200'>
           <AboutMe />
         </article>
 
-        <article>
+        <article className='d-400'>
           <MyProjects />
         </article>
 
-        <article>
+        <article className='d-400'>
           <MyStack />
         </article>
       </main>
